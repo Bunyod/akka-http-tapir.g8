2273a071dd0cda87de2736b5de6e29438952bfe8
Bunyod Bobojonov <bunyod.bobojonov@dreamlines.com>
2019-08-17 12:13:37 +0200

    optimize compiler options

--- src/main/g8/project/CompilerOptions.scala
+++ src/main/g8/project/CompilerOptions.scala
@@ -1,14 +1,15 @@
 object CompilerOptions {
 
   lazy val cOptions = Seq(
-    "-encoding", "utf8", // Option and arguments on same line
+    "-encoding", "utf8",// Specify character encoding used by source files.
     "-Xfatal-warnings",  // New lines for each options
-    "-deprecation",
-    "-unchecked",
-    "-language:implicitConversions",
-    "-language:higherKinds",
-    "-language:existentials",
-    "-language:postfixOps"
+    "-deprecation", // Emit warning and location for usages of deprecated APIs.
+    "-unchecked", // Enable additional warnings where generated code depends on assumptions.
+    "-language:implicitConversions", // Allow definition of implicit functions called views
+    "-language:higherKinds", // Allow higher-kinded types
+    "-language:existentials", // Existential types (besides wildcard types) can be written and inferred
+    "-Ypartial-unification", // Enable partial unification in type constructor inference
+
   )
 
 }
