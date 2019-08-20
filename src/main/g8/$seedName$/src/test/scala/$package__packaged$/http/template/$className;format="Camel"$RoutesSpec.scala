b1dc7e48ff691681937ecd821124f41cdec508ba
Bunyod Bobojonov <bunyod.bobojonov@dreamlines.com>
2019-08-20 23:24:08 +0200

    fix broken tests

--- src/main/g8/$seedName$/src/test/scala/$package__packaged$/http/template/$className;format="Camel"$RoutesSpec.scala
+++ src/main/g8/$seedName$/src/test/scala/$package__packaged$/http/template/$className;format="Camel"$RoutesSpec.scala
@@ -20,27 +20,31 @@ class $className;format="Camel"$RoutesSpec extends UnitSpec with AsyncMockFactor
 
   "The shout route" when {
     "called with 'GET /tweet/'" should {
-      "return with a HTTP status code of 200 and a JSON 'Seq[String]' response" is pendingUntilFixed {
+      "return with a HTTP status code of 200 and a JSON 'Seq[String]' response" in {
         val ctx = new Context with StubData
         import ctx._
 
         val mock: svc.Response[Seq[String]] = EitherT(
           Future(tweets.asRight[Error])
         )
-        (svc.tweets _).when("username", 10).returns(mock)
 
-        Get("/api/v1/tweet/username?limit=10") ~> rte ~> check {
+        (svc.tweets _).when("username", 10).returns(mock)
+        val authorization = OAuth2BearerToken("test")
+
+        Get("/api/v1/tweet/username?limit=10").addCredentials(authorization) ~> rte ~> check {
           status.intValue() shouldBe 200
           status shouldEqual StatusCodes.OK
         }
       }
 
-      "reject with a MissingQueryParamRejection for wrong field name" is pendingUntilFixed {
+      "return BadRequest for wrong field name" in {
         val ctx = new Context with StubData
         import ctx._
 
-        Get("/api/v1/tweet/username?limi=10") ~> rte ~> check {
-          rejection shouldBe an[MissingQueryParamRejection]
+        val authorization = OAuth2BearerToken("test")
+
+        Get("/api/v1/tweet/username?limi=10").addCredentials(authorization) ~> rte ~> check {
+          status shouldEqual StatusCodes.BadRequest
         }
       }
 
@@ -48,12 +52,16 @@ class $className;format="Camel"$RoutesSpec extends UnitSpec with AsyncMockFactor
         val ctx = new Context with StubData
         import ctx._
 
-        val mock: svc.Response[Seq[String]] = EitherT(Future(
-          Error(StatusCodes.InternalServerError.intValue, "Error occurred during getting tweets").asLeft[Seq[String]]
-        ))
+        val mock: svc.Response[Seq[String]] = EitherT(
+          Future(
+            Error(StatusCodes.InternalServerError.intValue, "Error occurred during getting tweets").asLeft[Seq[String]]
+          )
+        )
+        val authorization = OAuth2BearerToken("test")
+
         (svc.tweets _).when("username", 10).returns(mock)
 
-        Get("/api/v1/tweet/username?limit=10") ~> rte ~> check {
+        Get("/api/v1/tweet/username?limit=10").addCredentials(authorization) ~> rte ~> check {
           status shouldEqual StatusCodes.InternalServerError
           contentType shouldEqual ContentTypes.`application/json`
         }
