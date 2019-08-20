9687b49db0d7dea4bc4932b3fdf3d46f380d943f
Bunyod <bunyodreal@gmail.com>
2019-08-20 09:42:34 +0200

    Use endpoint descriptions to generate the routes
    * Use endpoint descriptions to generate the routes
    * commented broken tests

--- src/main/g8/$seedName$/src/main/scala/$package__packaged$/http/template/$className;format="Camel"$Routes.scala
+++ src/main/g8/$seedName$/src/main/scala/$package__packaged$/http/template/$className;format="Camel"$Routes.scala
@@ -2,55 +2,63 @@ package $package$
 package http
 package template
 
-import akka.http.scaladsl.server
-import akka.http.scaladsl.server.{Directives, RouteConcatenation}
-import cats.implicits._
+import akka.http.scaladsl.server.{Directives, Route, RouteConcatenation}
 import domain.template.{$className;format="Camel"$Service, Tweet}
 import http.auth.Authenticable
 import util.cfg.Configurable
 import util.json.FailFastCirceSupport
 import com.typesafe.scalalogging.LazyLogging
-import io.circe.generic.auto._
-import io.circe.syntax._
+import scala.concurrent.{ExecutionContext, Future}
 
-import scala.concurrent.ExecutionContext
+import tapir.server.akkahttp._
+import io.circe.generic.auto._
+import tapir.json.circe._
+import tapir.{endpoint, jsonBody, Endpoint, _}
 
 class $className;format="Camel"$Routes(svc: $className;format="Camel"$Service)(implicit ec: ExecutionContext)
-    extends Directives
-    with Configurable
-    with Authenticable
-    with FailFastCirceSupport
-    with RouteConcatenation
-    with LazyLogging {
+  extends Configurable with Authenticable with FailFastCirceSupport with LazyLogging with RouteConcatenation {
 
   import akka.http.scaladsl.server.{Directives => directive}
 
-  val route = pathPrefix("api") {
-    pathPrefix("v1") {
-      getTweets ~ addTweet
-    }
-  }
+  private val basePath = "api"
+  private val version = "v1"
 
-  def getTweets: server.Route = directive.get {
+  val route = getTweetsRoute ~ addTweetRoute
+
+  private def getTweets(username: String, limit: Int): Future[Either[Error, Seq[String]]] =
+    svc.tweets(username, limit).value
+
+  val getTweetsEndpoint: Endpoint[(String, String, Int), Error, Seq[String], Nothing] =
+    endpoint.get
+      .in(auth.bearer)
+      .in(basePath / version / "tweet")
+      .in(path[String]("username"))
+      .in(query[Int]("limit"))
+      .out(jsonBody[Seq[String]])
+      .errorOut(jsonBody[Error])
+
+  def getTweetsRoute: Route = directive.get {
     requireAuth { _: ApiClientIdentity =>
-      path("tweet" / Segment) { username =>
-        parameters('limit.as[Int]) { limit =>
-          complete(
-            svc.tweets(username, limit).fold(err => err.toHttpResponse, res => res.asJson.toHttpResponse)
-          )
-        }
+      getTweetsEndpoint.toRoute {
+        case (_, username, limit) =>
+        getTweets(username, limit)
       }
     }
   }
 
-  def addTweet: server.Route = directive.post {
-    path("tweet") {
-      entity(as[Tweet]) { twt =>
-        complete(
-          svc
-            .tweets(twt.text, 12)
-            .fold(err => err.toHttpResponse, res => res.asJson.toHttpResponse)
-        )
+  val addTweetEndpoint: Endpoint[(String, Tweet), Error, Seq[String], Nothing] =
+    endpoint.post
+      .in(auth.bearer)
+      .in(basePath / version / "tweet")
+      .in(jsonBody[Tweet])
+      .out(jsonBody[Seq[String]])
+      .errorOut(jsonBody[Error])
+
+  def addTweetRoute: Route = directive.post {
+    requireAuth { _: ApiClientIdentity =>
+      addTweetEndpoint.toRoute {
+        case (_, twt) =>
+        svc.tweets(twt.text, 12).value: Future[Either[Error, Seq[String]]]
       }
     }
   }
